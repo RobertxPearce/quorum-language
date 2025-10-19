2c077859a21e53b1bf822d1396ea25ccb1b06f17
alexgauthier65 <116533945+alexgauthier65@users.noreply.github.com>
2025-10-19 15:26:37 -0700

    Working on making keyboard work with textboxes in TextBoxIOS.java. Code needs further testing, may need to check that the characters are actually being updated on the screen.

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/TextBoxIOS.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/TextBoxIOS.java
@@ -2,12 +2,14 @@ package plugins.quorum.Libraries.Interface.Accessibility.IOS;
 
 import org.robovm.apple.uikit.UIAccessibilityContainer;
 import org.robovm.apple.uikit.UIAccessibilityTraits;
+import org.robovm.apple.uikit.UIKeyInput;
 import plugins.quorum.Libraries.Game.GameStateManager;
 import plugins.quorum.Libraries.Game.IOSInput;
 import quorum.Libraries.Interface.Controls.TextBox_;
 import quorum.Libraries.Interface.Controls.TextField_;
 
-public class TextBoxIOS extends TextAdjust{
+public class TextBoxIOS extends TextAdjust implements UIKeyInput {
+
     public boolean isAccessibilityElement() {
         return true;
     }
@@ -26,12 +28,16 @@ public class TextBoxIOS extends TextAdjust{
         IOSInput input = ((quorum.Libraries.Game.IOSInput) GameStateManager.input).plugin_;
         input.setFocusedTextAdjust(this);
         input.setOnscreenKeyboardVisible(true);
+
+        becomeFirstResponder();
     }
 
     public void FocusLost() {
         IOSInput input = ((quorum.Libraries.Game.IOSInput) GameStateManager.input).plugin_;
         input.setFocusedTextAdjust(null);
         input.setOnscreenKeyboardVisible(false);
+
+        resignFirstResponder();
     }
 
     public TextBox_ GetTextBox() {
@@ -40,12 +46,22 @@ public class TextBoxIOS extends TextAdjust{
     }
 
     @Override
-    public void Insert(String string) {
+    public boolean canBecomeFirstResponder() {
+        return true;
+    }
+
+    @Override
+    public boolean hasText(){
+        return GetText() != null && !GetText().isEmpty();
+    }
+
+    @Override
+    public void insertText(String string) {
         GetTextBox().Insert(string);
     }
 
     @Override
-    public void DeleteBackward() {
+    public void deleteBackward() {
         GetTextBox().DeleteBackward();
     }
 
