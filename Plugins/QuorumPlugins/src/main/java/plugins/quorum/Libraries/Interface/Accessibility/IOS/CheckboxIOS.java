f3f7a4355223007194310322e42e0bb7e50cd3ff
Nirbhay <nirbhayaggarwal6036@gmail.com>
2025-10-19 22:14:01 +0000

    Collaborative work, Nirbhay and Aidan, working on fixing checkbox functionaly with screen reader active.

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/CheckboxIOS.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/CheckboxIOS.java
@@ -28,19 +28,45 @@ public class CheckboxIOS extends ItemIOS {
         super.Initialize(checkbox);
     }
 
+
+    //worked on this together: Aidan and Nirbhay. Need to test before pushing.
+    /*Current objective(s):
+        -Checkbox doesn't uncheck with screen reader
+     */
     @Override
     public boolean activate() {
+        //create boolean for current state
         boolean isToggled = checkbox.GetToggleState();
-        if (isToggled == true) {
-            checkbox.Activate();
-            checkbox.SetToggleState(false);
-            checkbox.ClickedMouse();
-            return false;
-        } else {
-            checkbox.Activate();
-            checkbox.SetToggleState(true);;
-            checkbox.ClickedMouse();
-            return true;
-        }
+        //other boolean for readability
+        boolean oppositeState = !isToggled;
+        //set the toggle state to opposite of current
+        checkbox.SetToggleState(oppositeState);
+        //click the mouse
+        checkbox.ClickedMouse();
+        //set the traits and value depending on toggled or not
+        setAccessibilityTraits(oppositeState ? UIAccessibilityTraits.Selected : UIAccessibilityTraits.None);
+        setAccessibilityValue(oppositeState ? "Checked" : "Unchecked");
+        //post the notification
+        UIAccessibility.postNotification(UIAccessibility.Notification.Announcement, new NSString(oppositeState ? "Checked" : "Unchecked"));
+    //return opposite state
+    return oppositeState;
     }
+//
+//    ***OLD CODE FOR REFERENCE***
+//
+//    @Override
+//    public boolean activate() {
+//        boolean isToggled = checkbox.GetToggleState();
+//        if (isToggled == true) {
+//            checkbox.Activate();
+//            checkbox.SetToggleState(false);
+//            checkbox.ClickedMouse();
+//            return false;
+//        } else {
+//            checkbox.Activate();
+//            checkbox.SetToggleState(true);;
+//            checkbox.ClickedMouse();
+//            return true;
+//        }
+//    }
 }
