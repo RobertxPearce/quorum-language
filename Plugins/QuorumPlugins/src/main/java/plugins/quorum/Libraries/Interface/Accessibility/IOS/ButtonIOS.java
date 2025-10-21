95e4d7263e6b8f9a2d7bb37dc3c0daf04d8d9edc
alexgauthier65 <116533945+alexgauthier65@users.noreply.github.com>
2025-10-20 17:57:32 -0700

    Working on focus issue with buttons in ButtonIOS.java. Added a focus and focus lost function, needs further testing to confirm it works correctly.

--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/ButtonIOS.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/ButtonIOS.java
@@ -2,8 +2,9 @@ package plugins.quorum.Libraries.Interface.Accessibility.IOS;
 
 import org.robovm.apple.uikit.*;
 import quorum.Libraries.Interface.Controls.Button_;
+import plugins.quorum.Libraries.Game.GameStateManager;
 import quorum.Libraries.Interface.Item_;
-
+import plugins.quorum.Libraries.Game.IOSInput;
 
 public class ButtonIOS extends ItemIOS {
     @Override
@@ -17,6 +18,16 @@ public class ButtonIOS extends ItemIOS {
     }
 
     Button_ button;
+
+    public void Focus(){
+        IOSInput input = ((quorum.Libraries.Game.IOSInput) GameStateManager.input).plugin;
+        input.AddSingleTapEvent(this); // have to figure out what to pass to function
+    }
+
+    public void FocusLost(){
+        IOSInput input = ((quorum.Libraries.Game.IOSInput) GameStateManager.input).plugin;
+        input.AddSingleTapEvent(null);
+    }
 
     public void SetButton(Button_ button)
     {
--- Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/TextBoxIOS.java
+++ Plugins/QuorumPlugins/src/main/java/plugins/quorum/Libraries/Interface/Accessibility/IOS/TextBoxIOS.java
@@ -2,14 +2,12 @@ package plugins.quorum.Libraries.Interface.Accessibility.IOS;
 
 import org.robovm.apple.uikit.UIAccessibilityContainer;
 import org.robovm.apple.uikit.UIAccessibilityTraits;
-import org.robovm.apple.uikit.UIKeyInput;
 import plugins.quorum.Libraries.Game.GameStateManager;
 import plugins.quorum.Libraries.Game.IOSInput;
 import quorum.Libraries.Interface.Controls.TextBox_;
 import quorum.Libraries.Interface.Controls.TextField_;
 
-public class TextBoxIOS extends TextAdjust implements UIKeyInput {
-
+public class TextBoxIOS extends TextAdjust{
     public boolean isAccessibilityElement() {
         return true;
     }
@@ -28,16 +26,12 @@ public class TextBoxIOS extends TextAdjust implements UIKeyInput {
         IOSInput input = ((quorum.Libraries.Game.IOSInput) GameStateManager.input).plugin_;
         input.setFocusedTextAdjust(this);
         input.setOnscreenKeyboardVisible(true);
-
-        becomeFirstResponder();
     }
 
     public void FocusLost() {
         IOSInput input = ((quorum.Libraries.Game.IOSInput) GameStateManager.input).plugin_;
         input.setFocusedTextAdjust(null);
         input.setOnscreenKeyboardVisible(false);
-
-        resignFirstResponder();
     }
 
     public TextBox_ GetTextBox() {
@@ -46,22 +40,12 @@ public class TextBoxIOS extends TextAdjust implements UIKeyInput {
     }
 
     @Override
-    public boolean canBecomeFirstResponder() {
-        return true;
-    }
-
-    @Override
-    public boolean hasText(){
-        return GetText() != null && !GetText().isEmpty();
-    }
-
-    @Override
-    public void insertText(String string) {
+    public void Insert(String string) {
         GetTextBox().Insert(string);
     }
 
     @Override
-    public void deleteBackward() {
+    public void DeleteBackward() {
         GetTextBox().DeleteBackward();
     }
 
